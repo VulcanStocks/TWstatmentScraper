8d800927bb276312540ddd571ccfceda70f8b9fa
21ulwe <21ulwe@stockholmscience.se>
2023-01-13 16:42:47 +0100

    -

--- Scraper.cs
+++ Scraper.cs
@@ -3,6 +3,8 @@ using HtmlAgilityPack;
 using PuppeteerSharp;
 using System.IO;
 using TWscraper.Models;
+using System.Net;
+using System.Text.RegularExpressions;
 
 
 
@@ -14,13 +16,17 @@ public class Scraper
     private HtmlDocument doc { get; set; }
     private HtmlNodeCollection values { get; set; }
     private HtmlNodeCollection titles { get; set; }
-   private bool annual { get; set; }
-    private Parser parser { get; set; }
+    private bool annual { get; set; }
+    public List<StamentModel> dataRows { get; set; }
+    public bool UsePrefix { get; set; }
 
-    public void Initialize(string dataType, string ticker, bool annual)
+    public void Initialize(string dataType, string ticker, bool annual, bool UsePrefix)
     {
         xpath = "//div[contains(@class, 'value-pg2GO866')]";
         xpathTitles = "//span[@class='titleText-_PBNXQ7k']";
+        this.UsePrefix= UsePrefix;
+        dataRows = new List<StamentModel>();
+
 
         this.annual = annual;
 
@@ -41,17 +47,6 @@ public class Scraper
         }
     }
 
-    public void InitializeParser(bool usePrefix)
-    {
-        try
-        {
-            parser = new Parser(values, titles, usePrefix);
-        }
-        catch (Exception)
-        {
-            throw;
-        }
-    }
     public async Task LoadHtmlAsync()
     {
         var htmlAsTask = await LoadAndWaitForSelector(Url, ".value-pg2GO866");
@@ -103,33 +98,120 @@ public class Scraper
 
     public async Task ParseStatmentAsync()
     {
-        await parser.ParseIncomeAsync();
+        await ParseIncomeAsync();
     }
 
     public async Task SaveIncomeAsyncToCsv(string path)
     {
-        await parser.SaveIncomeAsync(path);
+        await SaveIncomeAsync(path);
     }
 
     public async Task<string[][]> GetStatmentArrayAsync()
     {
-        var statment = await parser.GetStatmentAsync();
-        string[][] statmentArray= new string[statment.Count][];
+        var statment = await GetStatmentAsync();
+        string[][] statmentArray = new string[statment.Count][];
         for (int i = 0; i < statment.Count(); i++)
         {
-            statmentArray[i] = new string[statment[i].columns.Count()+1];
+            statmentArray[i] = new string[statment[i].columns.Count() + 1];
             statmentArray[i][0] = statment[i].titleText;
             for (int j = 0; j < statment[i].columns.Count(); j++)
             {
-                statmentArray[i][j+1] = statment[i].columns[j];
+                statmentArray[i][j + 1] = statment[i].columns[j];
             }
         }
         return statmentArray;
     }
 
-    public async Task<List<StamentModel>> GetStatmentAsync()
+    public Task<List<StamentModel>> GetStatmentAsync()
     {
-        return await parser.GetStatmentAsync();
+        return Task.FromResult(dataRows);
+    }
+
+    public Task ParseIncomeAsync()
+    {
+        List<string> columns = new List<string>();
+
+        int count = 0;
+        int titleCount = 0;
+        bool start = false;
+
+        foreach (var item in values)
+        {
+            string nodeText = WebUtility.HtmlDecode(item.InnerText.ToString());
+
+
+            if (UsePrefix)
+            {
+                nodeText = Regex.Replace(nodeText, "[^0-9.KMB]", "");
+            }
+            else
+            {
+                nodeText = Regex.Replace(nodeText, "[^0-9.-]", "");
+            }
+            if (start)
+            {
+                columns.Add(nodeText);
+            }
+
+            count++;
+            if (count == 8)
+            {
+
+                string titleText = "";
+                titleText = WebUtility.HtmlDecode(titles[titleCount].InnerText.ToString());
+
+
+                if (start)
+                {
+                    dataRows.Add(new StamentModel { titleText = titleText, columns = columns });
+                    columns = new List<string>();
+                    titleCount++;
+                }
+                else
+                {
+                    start = true;
+                }
+
+
+                count = 0;
+            }
+
+        }
+
+
+
+        return Task.CompletedTask;
+    }
+
+
+    public Task SaveIncomeAsync(string path)
+    {
+        try
+        {
+            StreamWriter writer = new StreamWriter(path);
+
+            foreach (var row in dataRows)
+            {
+                writer.Write(row.titleText + ",");
+
+                for (int i = 0; i < row.columns.Count; i++)
+                {
+                    writer.Write(row.columns[i] + ",");
+                }
+
+                writer.WriteLine();
+
+            }
+            writer.Close();
+            Console.WriteLine($"Saved to {path}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return Task.CompletedTask;
+
     }
 
 }
